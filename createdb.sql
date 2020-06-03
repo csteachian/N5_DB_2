7e01ee3703dd0a22757cd10cb7db0c90a8568ba9
Ian Simpson <gw18simpsoniandavid@glow.sch.uk>
2020-06-03 13:46:46 +0000

    Initial commit

---
+++ createdb.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer (
+  first_name TEXT(50),
+  surname TEXT(50),
+  address TEXT(100),
+  date_joined DATE
+);
+
+INSERT INTO customer VALUES
+  ('Alan','Turing','Bletchley Park, Bletchley','2020-03-20');
---
+++ main.sql
@@ -0,0 +1,14 @@
+/* N5 Computing Science
+   Databases topic
+   I Simpson @familysimpson
+
+   Instructions:
+   Click "run" to view the results of the query.
+*/
+
+-- Do not change this
+.header on
+.mode column
+.read createdb.sql
+.read query.sql
+-- Thank you
